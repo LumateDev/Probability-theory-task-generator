911ade8489fd6170a8447dfd689246320013af0b
Alex <Sanchous557@mail.ru>
2023-04-10 00:44:06 +0300

    update design

--- src/App.java
+++ src/App.java
@@ -1,24 +1,65 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class App extends JFrame {
 
     App(){
-        super("Генератора задач на Теории Веростности");
+        super("Генератора задач по Теории вероятности");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         setContentPane(mainPanel);
-        setSize(720, 500);
+        setSize(720, 460);
         setResizable(false);
         setVisible(true);
-        labelTitle.setFont(new Font("Irial", Font.PLAIN, 28));
-        labelInputCountVar.setFont(new Font("Irial", Font.PLAIN, 24));
-        labelChoseTask.setFont(new Font("Irial", Font.PLAIN, 24));
+
+        topPanel.setBackground(new Color(194, 194, 194));
+        buttonPanel.setBackground(new Color(194, 194, 194));
+        lowPanel.setBackground(new Color(130,193,196));
+        mainPanel.setBackground(new Color(194, 194, 194));
+
+
+
+        labelInputCountVar.setFont(new Font("Segoe UI", Font.PLAIN, 24));
+
+        labelChoseTask.setFont(new Font("Segoe UI", Font.PLAIN, 24));
         labelChoseTask.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 0));
-        labelTitle.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 0));
         labelInputCountVar.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 0));
         checkAllTask.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+        checkAllTask.setFont(new Font("Segoe UI" , Font.PLAIN , 24));
+
+        class ColorListener implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                JButton button = (JButton)e.getSource();
+                if(button.getBackground() !=  new JButton().getBackground())
+                    button.setBackground(new JButton().getBackground());
+                else
+                    button.setBackground( new Color(66,144,224) );
+
+            }
+        }
+
+        ColorListener cl = new ColorListener();
+        buttonTask1.addActionListener(cl);
+        buttonTask2.addActionListener(cl);
+        buttonTask3.addActionListener(cl);
+        buttonTask4.addActionListener(cl);
+        buttonTask5.addActionListener(cl);
+        buttonTask6.addActionListener(cl);
+        buttonTask7.addActionListener(cl);
+        buttonTask8.addActionListener(cl);
+        buttonTask9.addActionListener(cl);
+        buttonTask10.addActionListener(cl);
+        buttonTask11.addActionListener(cl);
+        buttonTask12.addActionListener(cl);
+        buttonTask13.addActionListener(cl);
+        buttonTask14.addActionListener(cl);
+
+
+
+
 
     }
 
@@ -48,18 +89,20 @@ public class App extends JFrame {
     private JButton buttonCreateVar;
     private JCheckBox checkAllTask;
     private JLabel labelChoseTask;
+    private JPanel topPanel;
 
 
     public static void main(String[] args) {
         try {
             for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
+                if ("Nimbus".equals(info.getName())) {
                     UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
         } catch (Exception e) {
             setDefaultLookAndFeelDecorated(true);
+            e.printStackTrace();
         }
         new App();
     }
