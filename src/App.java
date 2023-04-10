d24af6271905a2f8969870b483e41fd88b40fd75
Alex <Sanchous557@mail.ru>
2023-04-10 08:21:24 +0300

    Update design

--- src/App.java
+++ src/App.java
@@ -1,3 +1,10 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
 import javax.swing.*;
 
 import java.awt.*;
@@ -23,13 +30,20 @@ public class App extends JFrame {
 
 
         labelInputCountVar.setFont(new Font("Segoe UI", Font.PLAIN, 24));
+        labelInputCountVar.setForeground(new Color(1,1,1));
+        labelInputCountVar.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 0));
 
         labelChoseTask.setFont(new Font("Segoe UI", Font.PLAIN, 24));
         labelChoseTask.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 0));
-        labelInputCountVar.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 0));
+        labelChoseTask.setForeground(new Color(1,1,1));
+
+        textFieldCountVar.setForeground(new Color(164, 164, 164));
+
+
         checkAllTask.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
         checkAllTask.setFont(new Font("Segoe UI" , Font.PLAIN , 24));
-
+        checkAllTask.setForeground(new Color(1,1,1));
+        buttonCreateVar.setFont(new Font("Segoe UI" , Font.PLAIN , 20));
         class ColorListener implements ActionListener {
             public void actionPerformed(ActionEvent e) {
                 JButton button = (JButton)e.getSource();
@@ -43,19 +57,33 @@ public class App extends JFrame {
 
         ColorListener cl = new ColorListener();
         buttonTask1.addActionListener(cl);
+        buttonTask1.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask2.addActionListener(cl);
+        buttonTask2.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask3.addActionListener(cl);
+        buttonTask3.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask4.addActionListener(cl);
+        buttonTask4.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask5.addActionListener(cl);
+        buttonTask5.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask6.addActionListener(cl);
+        buttonTask6.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask7.addActionListener(cl);
+        buttonTask7.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask8.addActionListener(cl);
+        buttonTask8.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask9.addActionListener(cl);
+        buttonTask9.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask10.addActionListener(cl);
+        buttonTask10.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask11.addActionListener(cl);
+        buttonTask11.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask12.addActionListener(cl);
+        buttonTask12.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask13.addActionListener(cl);
+        buttonTask13.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
         buttonTask14.addActionListener(cl);
+        buttonTask14.setFont(new Font("Segoe UI" , Font.PLAIN , 18));
 
 
 
@@ -94,12 +122,9 @@ public class App extends JFrame {
 
     public static void main(String[] args) {
         try {
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
+
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+
         } catch (Exception e) {
             setDefaultLookAndFeelDecorated(true);
             e.printStackTrace();
